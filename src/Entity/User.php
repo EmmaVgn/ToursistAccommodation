4f4dbc554b300f72e2461daf1873c94e7f200ed4
EmmaVgn <emma.vigneron@outlook.fr>
2024-05-15 15:01:28 +0200

    Initialize reset password

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -65,6 +65,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 60)]
     private ?string $phone = null;
 
+    #[ORM\Column(type: 'string', length: 100)]
+    private $resetToken;
+
     /**
      * @var Collection<int, Review>
      */
@@ -263,6 +266,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPhone(string $phone): static
     {
         $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getResetToken(): ?string
+    {
+        return $this->resetToken;
+    }
+
+    public function setResetToken(?string $resetToken): self
+    {
+        $this->resetToken = $resetToken;
 
         return $this;
     }
