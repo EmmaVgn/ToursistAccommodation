734963bf833f9bcf208d222790a1246376dcb7bc
EmmaVgn <emma.vigneron@outlook.fr>
2024-05-17 15:21:53 +0200

    Update homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AddRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(AddRepository $addRepository): Response
     {
+        $add = $addRepository->findAll();
+
         return $this->render('home/index.html.twig', [
+            'add' => $add,
             'controller_name' => 'HomeController',
         ]);
     }
